92aeaa9b01376eaeacde6b705d34ee6fc1200476
mariangel-ms <mariangelmartinez982@gmail.com>
2026-03-24 15:53:09 -0400

    second ocmmit

--- script.js
+++ script.js
@@ -1,49 +1,100 @@
-const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/gm;
+const PASSWORD_REGEX = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,16}$/gm;
 const USERNAME_REGEX = /^[a-zA-Z0-9]{5,16}$/;
-const EMAIL_REGEX =/([\w\.\-_]+)?\w+@[\w-_]+(\.\w+){1,}/igm;
+const EMAIL_REGEX = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 const NUMBER_REGEX = /^\s*(?:\+?(\d{1,3}))?([-. (]*(\d{3})[-. )]*)?((\d{3})[-. ]*(\d{2,4})(?:[-.x ]*(\d+))?)\s*$/gm;
 
-const countries = document.querySelector('#countries');
+const countries = document.querySelector("#countries");
 const username = document.querySelector("#username");
-const information = document.querySelector(".information");
 const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const phoneCode = document.querySelector("#phone-code");
+const number = document.querySelector("#phone");
+const confirmPassword = document.querySelector("#confirm-password");
 
-[...countries].forEach(country => {
-country.innerHTML= country.innerHTML.split('(')[0]
+[...countries].forEach((country) => {
+  country.innerHTML = country.innerHTML.split("(")[0];
 });
 
-let usernameValidation = false;
-let emailValidation = false;
-
-information.classList.add("show-information")
-
 //VALIDATION
-const validation = (e, validation, element) => {
-     if (validation) {
-element.classList.add('true');
-    element.classList.remove('false');
-    information.classList.add("show-information")
+const validation = (event, element, regex) => {
+  const validationTest = regex.test(element.value);
+  console.log(element.value);
 
-  } if(usernameValidation == false) {
-    element.classList.add('false');
-    element.classList.remove('true');
-    information.classList.remove("show-information")
- }
-}
+  //eto tengo que leerlo bien
+  let information = element.parentElement.querySelector(".information");
+  information = element.parentElement.children[2];
+  //---
+
+  if (validationTest) {
+    element.classList.add("true");
+    element.classList.remove("false");
+    information.classList.remove("show-information");
+  }
+  if (validationTest == false) {
+    element.classList.add("false");
+    element.classList.remove("true");
+    information.classList.add("show-information");
+  }
+
+  if (element.value == "") {
+    element.classList.remove("true");
+    element.classList.remove("false");
+    information.classList.remove("show-information");
+  }
+};
+
+countries.addEventListener("input", (event) => {
+  const optionSelected = [...event.target.children].find(
+    (option) => option.selected,
+  );
+  console.log(optionSelected);
+
+  phoneCode.innerHTML = `+ ${optionSelected.value}`;
+});
 
 //USERNAME
-username.addEventListener("input", event => {
- usernameValidation = USERNAME_REGEX.test(event.target.value);
-validation(event, usernameValidation, username)
-
-  if(event.target.value == "") {
-    username.classList.remove('true');
-    username.classList.remove('false');
-        information.classList.add("show-information")
- }
-})
+username.addEventListener("input", (event) => {
+  validation(event, username, USERNAME_REGEX);
+});
 
 //EMAIL
-email.addEventListener("input", event => {
- emailValidation = EMAIL_REGEX.test(event.target.value);
-})
+email.addEventListener("input", (event) => {
+  validation(event, email, EMAIL_REGEX);
+});
+
+//PHONE
+number.addEventListener("input", (event) => {
+  validation(event, number, NUMBER_REGEX);
+});
+
+//PASSWORD
+password.addEventListener("input", (event) => {
+  validation(event, password, PASSWORD_REGEX);
+
+ if (password.value !== confirmPassword.value) {
+   confirmPassword.classList.add("false");
+   confirmPassword.classList.remove("true");
+       information.classList.add("show-information");
+  }
+
+   if (password.value == confirmPassword.value) {
+   confirmPassword.classList.add("true");
+   confirmPassword.classList.remove("false");
+       information.classList.remove("show-information");
+ }
+});
+
+confirmPassword.addEventListener("input", (event) => {
+    validation(event, confirmPassword, PASSWORD_REGEX);
+ if (password.value == confirmPassword.value) {
+   confirmPassword.classList.add("true");
+   confirmPassword.classList.remove("false");
+       information.classList.remove("show-information");
+ }
+
+ if (password.value !== confirmPassword.value) {
+   confirmPassword.classList.add("false");
+   confirmPassword.classList.remove("true");
+       information.classList.add("show-information");
+  }
+});
